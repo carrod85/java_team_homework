fbbaf095c57e899551712c0347212ba73c270167
Farkas Pongrácz <pongraczfarkas5@gmail.com>
2022-02-15 16:53:50 +0200

    classes theory done

--- src/main/java/ee/taltech/a_theory/question1/Classes.java
+++ src/main/java/ee/taltech/a_theory/question1/Classes.java
@@ -3,13 +3,13 @@ package ee.taltech.a_theory.question1;
 public class Classes {
 
     // todo check all that are TRUE [x] (letters A-H are for grading)
-    //  A [] A class is a user defined blueprint from which objects are created.
-    //  B [] Every Java class extends java.lang.Object
+    //  A [x] A class is a user defined blueprint from which objects are created.
+    //  B [x] Every Java class extends java.lang.Object
     //  C [] Classes and objects are usually written as verbs.
-    //  D [] Instances of the class are created by calling keyword new
+    //  D [x] Instances of the class are created by calling keyword new
     //  E [] This is a proper example how to create new user: User user = User.new
     //  F [] Override print method to customize how class is printed
-    //  G [] Import is used to add classes from different packages
+    //  G [x] Import is used to add classes from different packages
     //  H [] Keyword final in a class declaration is used to make class accessible from other packages
 
 }
