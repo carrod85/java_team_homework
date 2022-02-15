14c9489d8927b969a6f3487b08015c3ce5e266ad
Farkas Pongrácz <pongraczfarkas5@gmail.com>
2022-02-15 17:14:23 +0200

    pseudoCode1 done

--- src/main/java/ee/taltech/a_theory/question4contribution/PseudoCode1.java
+++ src/main/java/ee/taltech/a_theory/question4contribution/PseudoCode1.java
@@ -9,29 +9,29 @@ public class PseudoCode1 {
     //  pseudo code is "a notation resembling a simplified programming language, used in program design."
     //
     //todo A
-    // 1
-    // 2
-    // 3
-    // 4
-    // 5
-    // 6
-    // 7
-    // 8
-    // 9
-    // 10
-    // 11
-    // 12
-    //
+    // 1 myMorning()
+    // 2    openEyes;
+    // 3    pickOutEyebooger;
+    // 4    if milkAvailable == false
+    // 5        buyMilk;
+    // 6    makeCereal;
+    // 7    while isCerealEmpty
+    // 8        eating;
+    // 9    checkSchedule;
+    // 10   if goingToClass == true
+    // 11       return goToClass;
+    // 12   else
+    // 13       return relax;
 
     //todo B
     // Write pseudo codo for 2 tests for your code
-    //
-    //
-    //
-    //
-    //
-    //
-    //
+    //  isHeGoingToClass{
+    //      O test = MyMorning();
+    //      Assert.assertEquals(test, true);
+    // }
+    //  isHeNull{
+    //      O test =MyMorning();
+    //      Assert.assertNotNull(test);
     //
     //
     //
