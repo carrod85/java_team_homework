5ed7cce97851fb60f5a6c374bfb32d918142968d
carrod <carrod@ttu.ee>
2022-02-19 10:07:36 +0200

    assigment 7 done

--- src/main/java/ee/taltech/b_practise/assignment7/Fullname.java
+++ src/main/java/ee/taltech/b_practise/assignment7/Fullname.java
@@ -7,8 +7,22 @@ public class Fullname {
     //todo C use streams (extra)
 
     public static String fullname(String firstName, String lastName) {
-        return "";
-    }
 
+        String name = firstName + " " + lastName;
+
+        if ((firstName == null || firstName.isEmpty()) && (lastName == null || lastName.isEmpty())) {
+            return "";
+        }
+        else if (lastName == null || lastName.isEmpty()) {
+            return firstName;
+        }
+        else if (firstName == null || firstName.isEmpty() ) {
+            return lastName;
+        }
+        else{
+            return name;
+        }
+
+    }
 
 }
