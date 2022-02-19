e79d0d8a8185b9b05f1a296e2c73c6f749eb9957
carrod <carrod@ttu.ee>
2022-02-19 10:42:59 +0200

    question 2 and question 3 done

--- src/main/java/ee/taltech/a_theory/question2/Functions.java
+++ src/main/java/ee/taltech/a_theory/question2/Functions.java
@@ -3,13 +3,13 @@ package ee.taltech.a_theory.question2;
 public class Functions {
 
     // todo check all that are TRUE [x] (letters A-H are for grading)
-    //  A [] function is a reusable piece of code that can take in variables and can return an output
-    //  B [] in java functions are different from methods as functions can be created separately while methods are connected to an object
-    //  C [] method names are usually written as verbs
+    //  A [x] function is a reusable piece of code that can take in variables and can return an output
+    //  B [x] in java functions are different from methods as functions can be created separately while methods are connected to an object
+    //  C [x] method names are usually written as verbs
     //  D [] static method means that the method returns no value
     //  E [] void methods can be called without creating a new instance of a class
     //  F [] supplier/generator is a type of function that takes in inputs and doesn't return anything
     //  G [] if you want your method to be used by other classes in other packages you declare it as protected
-    //  H [] private methods are accessible only inside a class
+    //  H [x] private methods are accessible only inside a class
 }
 
--- src/main/java/ee/taltech/a_theory/question3/Compilation.java
+++ src/main/java/ee/taltech/a_theory/question3/Compilation.java
@@ -5,7 +5,8 @@ public class Compilation {
     //todo A
     // in your own words (do not use wiki definitions)
     // What is compilation?
-    // Answer:
+    // Answer: the process of translating the language/code of Java into code understandble by the computer hardware to
+    // processed.
     //
     //
     //
@@ -13,22 +14,24 @@ public class Compilation {
     //todo B
     // Name 2 benefits of compilation?
     // Benefits:
-    // 1
-    // 2
+    // 1 The languages that need to be compiled are considered faster than interpreted languages because they are
+    // transfored into machine code so the code is optimized up to the maximum of the computer hardware ressources.
+    // 2 You generally have more control over the management of memory, cpu usage.
+    //
     //todo C
     // Name 1 disadvantage of compilation?
     // Disadvantage:
-    // 1
+    // 1 Prone to issues of compatibility between different computer's architecture.
     //todo D
     // Name 2x other languages using compiler
-    // 1
-    // 2
+    // 1 C
+    // 2 C++
     //todo E
     // What is opposite of compiled language?
-    // Answer: _____ language (1 word)
+    // Answer: interpreted language (1 word)
     //todo F
     // Name 2x languages of E
-    // 1
-    // 2
+    // 1 Java
+    // 2 Python
 
 }
