bb8dc402ad993440561c8bfe6252fe14c622fdd8
bekosz <bekosz@ttu.ee>
2022-02-19 14:43:25 +0200

    Filter Done

--- src/main/java/ee/taltech/b_practise/assignment8/Filter.java
+++ src/main/java/ee/taltech/b_practise/assignment8/Filter.java
@@ -1,5 +1,7 @@
 package ee.taltech.b_practise.assignment8;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Filter {
@@ -15,7 +17,29 @@ public class Filter {
      * divides integer input into positives and negatives
      * zero 0, can be treated as positive
      */
-    public PositivesAndNegatives filterToPositivesAndNegatives(List<Integer> numbers){
-        return new PositivesAndNegatives();
+    public static HashMap <String, List> filterToPositivesAndNegatives(List<Integer> numbers){
+        if (numbers == null || numbers.isEmpty()){
+            return null;
+        }
+
+        HashMap <String, List> positiveAndNegatives = new HashMap<String, List>();
+
+        List<Integer> positive = new ArrayList<>();
+        List<Integer> negative = new ArrayList<>();
+
+        for (int i = 0; i< numbers.size(); i++){
+            if (numbers.get(i)>= 0){
+                positive.add(numbers.get(i));
+            }
+            else{
+                negative.add(numbers.get(i));
+            }
+        }
+
+        positiveAndNegatives.put("positive", positive);
+        positiveAndNegatives.put("negative", negative);
+
+
+        return positiveAndNegatives;
     }
 }
--- src/test/java/ee/taltech/b_practise/assignment8/FilterTest.java
+++ src/test/java/ee/taltech/b_practise/assignment8/FilterTest.java
@@ -1,6 +1,32 @@
 package ee.taltech.b_practise.assignment8;
 
+import ee.taltech.b_practise.assignment4.PowerOf3;
+import ee.taltech.b_practise.assignment7.Fullname;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FilterTest {
 
+
     //todo write the tests you think are necessary :)
+    @Test
+    void check_positive() {
+        assertEquals(List.of(0,1,2), Filter.filterToPositivesAndNegatives(List.of(0,1,-1,2,-4)).get("positive"));
+    }
+    @Test
+    void check_negative() {
+        assertEquals(List.of(-1,-4), Filter.filterToPositivesAndNegatives(List.of(0,1,-1,2,-4)).get("negative"));
+    }
+    @Test
+    void check_null() {
+        assertEquals(null, Filter.filterToPositivesAndNegatives(null));
+    }
+
+    @Test
+    void check_empty() {
+        assertEquals(null, Filter.filterToPositivesAndNegatives(List.of()));
+    }
 }
