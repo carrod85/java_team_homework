da719b21ad29609bda17978f9782d601bee8eefe
bekosz <bekosz@ttu.ee>
2022-02-20 12:39:05 +0200

    DOne

--- src/main/java/ee/taltech/c_extra/problem1/BizarreMath.java
+++ src/main/java/ee/taltech/c_extra/problem1/BizarreMath.java
@@ -6,21 +6,21 @@ public class BizarreMath {
     // byte is a data structure from -128 to 127
     // can you explain in plain english (simpler the better)
     // why 127 + 1 = -128 ?! (you can run main method to see it for yourself)
-    // Answer:
+    // Answer:127+1 is 128 which is outside of the scope of the byte variable so its start from the beginning again
     //
     //
     //todo B
     // How is this phenomenon called? (this is unrelated, but funny https://www.youtube.com/watch?v=h5Mc55P1i9g)
-    // Answer (one word):
+    // Answer (one word):Overload
     //
     //todo C
     // What would you recommend developer to do?
-    // Answer:
+    // Answer:Use bigger scope
     //
     //
     //todo D
     // Does the similar thing happen with "int/Integer"?
-    // Answer: Yes/No
+    // Answer: Yes
 
     public static void main(String[] args) {
         byte num = 127;
